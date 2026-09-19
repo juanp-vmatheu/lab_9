8ea0f6095acd1c29a86e5831de8ffe42484a07fe
cuent <juan_valle@unitec.edu>
2026-09-19 09:34:55 -0600

    Operaciones de busqueda y eliminacion en la lista enlazada

--- src/paqueteria/ListaEnlazada.java
+++ src/paqueteria/ListaEnlazada.java
@@ -48,4 +48,76 @@ public class ListaEnlazada<T> {
     public boolean estaVacia() {
         return tamanio == 0;
     }
+
+    public boolean eliminar(T dato) {
+        if (cabeza == null) {
+            return false;
+        }
+        if (cabeza.getDato().equals(dato)) {
+            cabeza = cabeza.getSiguiente();
+            tamanio--;
+            return true;
+        }
+        Nodo<T> anterior = cabeza;
+        Nodo<T> actual = cabeza.getSiguiente();
+        while (actual != null) {
+            if (actual.getDato().equals(dato)) {
+                anterior.setSiguiente(actual.getSiguiente());
+                tamanio--;
+                return true;
+            }
+            anterior = actual;
+            actual = actual.getSiguiente();
+        }
+        return false;
+    }
+
+    public T eliminarEn(int indice) {
+        if (indice < 0 || indice >= tamanio) {
+            return null;
+        }
+        if (indice == 0) {
+            T dato = cabeza.getDato();
+            cabeza = cabeza.getSiguiente();
+            tamanio--;
+            return dato;
+        }
+        Nodo<T> anterior = cabeza;
+        for (int i = 0; i < indice - 1; i++) {
+            anterior = anterior.getSiguiente();
+        }
+        Nodo<T> aEliminar = anterior.getSiguiente();
+        anterior.setSiguiente(aEliminar.getSiguiente());
+        tamanio--;
+        return aEliminar.getDato();
+    }
+
+    public T buscar(T dato) {
+        Nodo<T> actual = cabeza;
+        while (actual != null) {
+            if (actual.getDato().equals(dato)) {
+                return actual.getDato();
+            }
+            actual = actual.getSiguiente();
+        }
+        return null;
+    }
+
+    public void vaciar() {
+        cabeza = null;
+        tamanio = 0;
+    }
+
+    public String recorrer() {
+        StringBuilder resultado = new StringBuilder();
+        Nodo<T> actual = cabeza;
+        while (actual != null) {
+            resultado.append(actual.getDato().toString());
+            if (actual.getSiguiente() != null) {
+                resultado.append(", ");
+            }
+            actual = actual.getSiguiente();
+        }
+        return resultado.toString();
+    }
 }
